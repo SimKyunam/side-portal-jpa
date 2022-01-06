d3d240d6662de0c473b93ab2d0fa1e2eec9a3e91
심규남 <ppp9026@gmail.com>
2022-01-06 17:21:07 +0900

    - WebClient 테스트 코드 작성

--- src/test/java/com/mile/portal/adaptor/hcmp/board/HcmpBoardTest.java
+++ src/test/java/com/mile/portal/adaptor/hcmp/board/HcmpBoardTest.java
@@ -1,13 +1,27 @@
 package com.mile.portal.adaptor.hcmp.board;
 
 import com.mile.portal.adaptor.hcmp.board.dto.HcmpBoardReq;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class HcmpBoardTest {
+
+    @Value("${hcmp.url}")
+    private String hcmpUrl;
 
     @Autowired
     private HcmpBoard hcmpBoard;
@@ -34,6 +48,25 @@ class HcmpBoardTest {
                 .build();
 
         hcmpBoard.createBoardQna(boardQna);
+    }
+
+    @Test
+    @DisplayName("4. 게시판 조회 - webClient")
+    void test4() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofMillis(5000))
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
+
+        WebClient client = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+
+        WebClient.RequestBodyUriSpec uriSpec = client.method(HttpMethod.POST);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(hcmpUrl + "/api/v1/board/createBoardQna");
+
     }
 
 }
