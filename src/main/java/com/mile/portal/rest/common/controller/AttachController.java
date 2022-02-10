a30bf798d3c6e35aaaa1db6cc1fdd254eb1bb5c7
심규남 <ppp9026@gmail.com>
2022-02-10 13:54:14 +0900

    zip 파일 다운로드 추가

--- src/main/java/com/mile/portal/rest/common/controller/AttachController.java
+++ src/main/java/com/mile/portal/rest/common/controller/AttachController.java
@@ -2,6 +2,7 @@ package com.mile.portal.rest.common.controller;
 
 import com.mile.portal.rest.common.model.domain.Attach;
 import com.mile.portal.rest.common.service.AttachService;
+import com.mile.portal.util.DateTimeUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
@@ -9,14 +10,18 @@ import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Slf4j
 @RestController
@@ -52,5 +57,46 @@ public class AttachController {
                 .contentType(MediaType.parseMediaType(contentType))
                 .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition.toString())
                 .body(file);
+    }
+
+    @GetMapping("/zipDownload")
+    public void zipDownload(@RequestParam Long id, @RequestParam String type, HttpServletResponse response) {
+        ZipOutputStream zipOut = null;
+        FileInputStream fis = null;
+
+        String zipName = DateTimeUtil.getToday("yyyyMMddHHmmss") + ".zip";
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/zip");
+        response.addHeader("Content-Disposition", "attachment;filename=" + zipName);
+
+        try {
+            zipOut = new ZipOutputStream(response.getOutputStream());
+            List<? extends Attach> attachFiles = attachService.listAttacheFile(id, type);
+
+            for (Attach attachFile : attachFiles) {
+                File file = new File(attachFile.getPath() + "/" + attachFile.getAttachUpload());
+
+                zipOut.putNextEntry(new ZipEntry(attachFile.getAttachDown()));
+                fis = new FileInputStream(file);
+                StreamUtils.copy(fis, zipOut);
+
+                fis.close();
+                zipOut.closeEntry();
+            }
+            zipOut.close();
+        } catch (IOException e) {
+            try {
+                if (fis != null) fis.close();
+            } catch (IOException e1) {
+                System.out.println(e1.getMessage());/*ignore*/
+            }
+            try {
+                zipOut.closeEntry();
+                zipOut.close();
+            } catch (IOException e2) {
+                System.out.println(e2.getMessage());/*ignore*/
+            }
+        }
     }
 }
--- src/main/java/com/mile/portal/rest/common/service/AttachService.java
+++ src/main/java/com/mile/portal/rest/common/service/AttachService.java
@@ -4,9 +4,12 @@ import com.mile.portal.config.exception.exceptions.ResultNotFoundException;
 import com.mile.portal.rest.base.service.BaseFileService;
 import com.mile.portal.rest.common.model.domain.Attach;
 import com.mile.portal.rest.common.repository.AttachRepository;
+import com.mile.portal.rest.common.repository.BoardAttachRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -14,9 +17,16 @@ import org.springframework.stereotype.Service;
 public class AttachService extends BaseFileService {
 
     private final AttachRepository attachRepository;
+    private final BoardAttachRepository boardAttachRepository;
 
     //파일 다운로드
     public Attach downloadFile(String fileName) {
         return attachRepository.findByAttachUpload(fileName).orElseThrow(ResultNotFoundException::new);
+    }
+
+    //파일 다운로드
+    public List<? extends Attach> listAttacheFile(Long id, String type) {
+        return boardAttachRepository.findByBoardIdAndBoardType(id, type)
+                .orElseThrow(ResultNotFoundException::new);
     }
 }
